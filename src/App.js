7676f1da585ec2024838e408a95b0be870a8a098
Jacob <Jacob.K289@outlook.com>
2023-05-17 02:19:23 +0200

    added global state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import Header from "./components/Header";
-import FeedBackItem from "./components/FeedBackItem";
+import FeedBackList from "./components/FeedBackList";
+import FeedbackData from "./data/FeedBackData";
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
   return (
     <>
       <Header />
       <div className="container">
-        <FeedBackItem />
+        <FeedBackList feedback={feedback} />
       </div>
     </>
   );
---
+++ src/components/FeedBackList.jsx
@@ -0,0 +1,16 @@
+import FeedBackItem from "./FeedBackItem";
+
+function FeedBackList({ feedback }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback yet</p>;
+  }
+  return (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedBackItem key={item.id} item={item} />
+      ))}
+    </div>
+  );
+}
+
+export default FeedBackList;
